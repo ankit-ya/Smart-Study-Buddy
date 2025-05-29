b49f42cc7fb3ede92dfb9f3204edb37346fffe5d
ankit-ya <ankit3001yadav@gmail.com>
2025-05-30 01:44:54 +0530

    adds

--- backend/index.js
+++ backend/index.js
@@ -58,6 +58,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 
 // Start server on port 5000
-server.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
